61e1ef2cb101ed1768937a7856c3a846e62fcb95
Hriscu Ovidiu <hriscuo@gmail.com>
2021-03-20 16:04:31 +0200

    Updated cognito with userId attribute

--- SeeYouRomania.Domain/Enums/ClaimNames.cs
+++ SeeYouRomania.Domain/Enums/ClaimNames.cs
@@ -2,8 +2,8 @@
 {
     public static class ClaimNames
     {
-        public static string UserId => "custom:custom:userId";
-        public static string UserType => "custom:custom:userType";
+        public static string UserId => "custom:userId";
+        public static string UserType => "custom:userType";
         public static string Email => "email";
     }
 }
